8f012e1281a827064521dffb4dbcf8ce906fb348
Ugur Polat <ugurpolat113@gmail.com>
2021-10-12 09:32:04 +0300

    Added Roll button for refresh dice.

--- Dice Game/JS/index.js
+++ Dice Game/JS/index.js
@@ -1,32 +1,34 @@
 "use strict";
 
-var randomNumber1;
-var randomNumber2;
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var randomNumber1;
+  var randomNumber2;
 
-randomNumber1 = Math.ceil(Math.random() * 6);
+  randomNumber1 = Math.ceil(Math.random() * 6);
 
-var randomDiceImage = "dice" + randomNumber1 + ".png";
+  var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var randomImageSource = "/Asset/" + randomDiceImage;
+  var randomImageSource = "/Dice Game/Asset/" + randomDiceImage;
 
-document
-  .querySelector(".left-dice")
-  .lastElementChild.setAttribute("src", randomImageSource);
+  document
+    .querySelector(".left-dice")
+    .lastElementChild.setAttribute("src", randomImageSource);
 
-randomNumber2 = Math.ceil(Math.random() * 6);
+  randomNumber2 = Math.ceil(Math.random() * 6);
 
-var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+  var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 
-var randomImageSource2 = "/Asset/" + randomDiceImage2;
+  var randomImageSource2 = "/Dice Game/Asset/" + randomDiceImage2;
 
-document
-  .querySelector(".right-dice")
-  .lastElementChild.setAttribute("src", randomImageSource2);
+  document
+    .querySelector(".right-dice")
+    .lastElementChild.setAttribute("src", randomImageSource2);
 
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerText = "Player 1 Wins";
-} else if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").innerText = "Player 2 Wins";
-} else {
-  document.querySelector("h1").innerText = "Draw";
-}
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerText = "Player 1 Wins";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerText = "Player 2 Wins";
+  } else {
+    document.querySelector("h1").innerText = "Draw";
+  }
+});
